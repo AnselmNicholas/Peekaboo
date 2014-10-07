d358d60a714e55b63418a59e539a798c3ddfd429
Xinlei's Air <caixl@live.com>
2014-10-07 20:49:24 +0800

    Camera Hook

---
+++ src/sg/edu/nus/soc/cs5231/CameraHooks.java
@@ -0,0 +1,28 @@
+package sg.edu.nus.soc.cs5231;
+
+import static de.robv.android.xposed.XposedHelpers.*;
+import de.robv.android.xposed.*;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import static de.robv.android.xposed.XposedBridge.*;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+
+public class CameraHooks implements IXposedHookLoadPackage {
+	
+		public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+				hookCamera(lpparam);
+		}
+			
+		private void hookCamera(final LoadPackageParam lpparam)
+		{
+			final Class<?> classFinder = findClass("android.hardware.Camera", lpparam.classLoader);
+			hookAllConstructors(classFinder, new XC_MethodHook() { 
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					XposedBridge.log("Xinlei: " + lpparam.processName + " trying to access camera.");
+				}
+			});
+		}
+		
+}
