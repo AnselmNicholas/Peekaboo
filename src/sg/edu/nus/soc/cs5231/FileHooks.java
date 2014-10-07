e308d1ec060d78b60b4802dc116b533f685863a7
Jason Poh <jason@codeit.sg>
2014-10-07 16:08:19 +0800

    Added file/dir hooks for rename, delete and create.

--- src/sg/edu/nus/soc/cs5231/FileHooks.java
+++ src/sg/edu/nus/soc/cs5231/FileHooks.java
@@ -8,18 +8,62 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 
 public class FileHooks implements IXposedHookLoadPackage {
+		static String [] whiteList = { "com.google.android.gms:snet" };
 		public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 				XposedBridge.log(lpparam.packageName + " is loaded...");
+				if( IsInWhiteList(lpparam.processName) )
+				{
+					return;
+				}
 				hookListFiles(lpparam);
+				hookRenameFile(lpparam);
+				hookDeleteFile(lpparam);
+				hookCreateFile(lpparam);
 		}
 		
+		private void hookCreateFile(final LoadPackageParam lpparam)
+		{
+			findAndHookMethod("java.io.File", lpparam.classLoader, "createNewFile", new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					String fileName = ((java.io.File)param.thisObject).getAbsolutePath();
+					XposedBridge.log("Jason: " + lpparam.processName + " creating new file " + fileName + ".");
+				}
+			});
+		}
+		
+		private void hookDeleteFile(final LoadPackageParam lpparam)
+		{
+			findAndHookMethod("java.io.File", lpparam.classLoader, "delete", new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					String fileName = ((java.io.File)param.thisObject).getAbsolutePath();
+					XposedBridge.log("Jason: " + lpparam.processName + " deleting " + fileName + ".");
+				}
+			});
+			findAndHookMethod("java.io.File", lpparam.classLoader, "deleteOnExit", new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					String fileName = ((java.io.File)param.thisObject).getAbsolutePath();
+					XposedBridge.log("Jason: " + lpparam.processName + " deleting " + fileName + ".");
+				}
+			});
+		}
+		
+		private void hookRenameFile(final LoadPackageParam lpparam) {
+			findAndHookMethod("java.io.File", lpparam.classLoader, "renameTo", java.io.File.class, new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					String originalName = ((java.io.File)param.thisObject).getAbsolutePath();
+					String newName = ((java.io.File)param.args[0]).getAbsolutePath();
+					XposedBridge.log("Jason: " + lpparam.processName + " renaming " + originalName + " to " + newName +".");
+				}
+			});
+			
+		}
+
 		private void hookListFiles(final LoadPackageParam lpparam)
 		{
-			String [] whiteList = { "com.google.android.gms:snet" };
-			if( IsInWhiteList(whiteList,lpparam.processName) )
-			{
-				return;
-			}
 			findAndHookMethod("java.io.File", lpparam.classLoader, "listFiles", new XC_MethodHook() {
 				@Override
 				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
@@ -29,7 +73,7 @@ public class FileHooks implements IXposedHookLoadPackage {
 			});
 		}
 		
-		private static boolean IsInWhiteList(String [] whiteList, String target)
+		private static boolean IsInWhiteList(String target)
 		{
 			for(String str : whiteList)
 			{
