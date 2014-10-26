4a8e8131ff9bac21a4aa7aa55015ac03bf3bda15
Xinlei's Air <caixl@live.com>
2014-10-26 21:12:58 +0800

    CalendarHooks (ContentProvider)

--- src/sg/edu/nus/soc/cs5231/AccountHooks.java
+++ src/sg/edu/nus/soc/cs5231/AccountHooks.java
@@ -1,6 +1,5 @@
 package sg.edu.nus.soc.cs5231;
 
-import static de.robv.android.xposed.XposedBridge.hookAllMethods;
 import static de.robv.android.xposed.XposedHelpers.*;
 import de.robv.android.xposed.*;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
@@ -10,16 +9,14 @@ public class AccountHooks implements IXposedHookLoadPackage {
 	
 		public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 				hookAccount(lpparam);
-				
-				
 		}
 			
 		private void hookAccount(final LoadPackageParam lpparam)
 		{
 
 			final Class<?> classFinder = findClass("android.accounts.AccountManager", lpparam.classLoader);
-				
-			hookAllMethods(classFinder,"getAccounts", new XC_MethodHook() { 
+			
+			findAndHookMethod(classFinder,"getAccounts", new XC_MethodHook() { 
 				@Override
 				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 					Logger.Log(lpparam.processName, "AccountManager", "getAccounts()", "trying to get account info.");
---
+++ src/sg/edu/nus/soc/cs5231/CalendarHooks.java
@@ -0,0 +1,32 @@
+package sg.edu.nus.soc.cs5231;
+
+import static de.robv.android.xposed.XposedHelpers.*;
+import android.net.Uri;
+import android.os.CancellationSignal;
+import android.provider.CalendarContract.Calendars;
+import de.robv.android.xposed.*;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+
+public class CalendarHooks implements IXposedHookLoadPackage {
+	
+		public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+				hookCalendar(lpparam);
+		}
+			
+		private void hookCalendar(final LoadPackageParam lpparam)
+		{
+			final Class<?> classFinder = findClass("android.content.ContentResolver", lpparam.classLoader);
+			XposedBridge.hookMethod(findMethodBestMatch(classFinder,"query", Uri.class, String[].class, String.class, String[].class, 
+                    String.class, CancellationSignal.class ), 
+                    new XC_MethodHook() { 
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					Object uri = param.args[0];
+					if(uri.equals(Calendars.CONTENT_URI))
+						Logger.Log(lpparam.processName, "ContentResolver", "query()", " "+uri);
+				}
+			});
+		}
+		
+}
