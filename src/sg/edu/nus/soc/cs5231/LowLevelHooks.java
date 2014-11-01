69098eb34e173b2a83d9c488daecacc4edea5afc
Jason Poh <jason@codeit.sg>
2014-11-01 10:54:55 +0800

    Added LowLevelHooks for loadLibrary, load & exec,

---
+++ src/sg/edu/nus/soc/cs5231/LowLevelHooks.java
@@ -0,0 +1,107 @@
+package sg.edu.nus.soc.cs5231;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+import java.io.File;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class LowLevelHooks implements IXposedHookLoadPackage {
+	static String [] whiteList = { 	"jp.naver.line.android"	};
+	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+		if( !IsInWhiteList(lpparam.processName) )
+		{
+			return;
+		}
+		hookLoadSharedLibrary(lpparam);
+		hookExec(lpparam);
+	}
+	
+	private void hookLoadSharedLibrary(final LoadPackageParam lpparam)
+	{
+		findAndHookMethod("java.lang.System", lpparam.classLoader, "loadLibrary", String.class, new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				Logger.Log(lpparam.processName, "java.lang.System", "loadLibrary()", "trying to load shared library \"" + param.args[0] + "\"");
+			}
+		});
+		findAndHookMethod("java.lang.System", lpparam.classLoader, "load", String.class, new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				Logger.Log(lpparam.processName, "java.lang.System", "loadLibrary()", "trying to load shared library \"" + param.args[0] + "\"");
+			}
+		});
+	}
+	
+	private void hookExec(final LoadPackageParam lpparam)
+	{
+		findAndHookMethod("java.lang.Runtime", lpparam.classLoader, "exec", String.class, new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				Logger.Log(lpparam.processName, "java.lang.Runtime", "exec()", "trying to exec command \"" + param.args[0] + "\"");
+			}
+		});
+		findAndHookMethod("java.lang.Runtime", lpparam.classLoader, "exec", String[].class, new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				String[] args = (String[])param.args[0];
+				String cmd = "";
+				for(String s : args)
+				{
+					cmd = cmd + " " + s;
+				}
+				Logger.Log(lpparam.processName, "java.lang.Runtime", "exec()", "trying to exec command \"" + cmd + "\"");
+			}
+		});
+		findAndHookMethod("java.lang.Runtime", lpparam.classLoader, "exec", String.class, String[].class, new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				Logger.Log(lpparam.processName, "java.lang.Runtime", "exec()", "trying to exec command \"" + param.args[0] + "\"");
+			}
+		});
+		findAndHookMethod("java.lang.Runtime", lpparam.classLoader, "exec", String.class, String[].class, File.class, new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				Logger.Log(lpparam.processName, "java.lang.Runtime", "exec()", "trying to exec command \"" + param.args[0] + "\"");
+			}
+		});
+		findAndHookMethod("java.lang.Runtime", lpparam.classLoader, "exec", String[].class, String[].class, File.class, new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				String[] args = (String[])param.args[0];
+				String cmd = "";
+				for(String s : args)
+				{
+					cmd = cmd + " " + s;
+				}
+				Logger.Log(lpparam.processName, "java.lang.Runtime", "exec()", "trying to exec command \"" + cmd + "\"");
+			}
+		});
+		findAndHookMethod("java.lang.Runtime", lpparam.classLoader, "exec", String[].class, String[].class, new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				String[] args = (String[])param.args[0];
+				String cmd = "";
+				for(String s : args)
+				{
+					cmd = cmd + " " + s;
+				}
+				Logger.Log(lpparam.processName, "java.lang.Runtime", "exec()", "trying to exec command \"" + cmd + "\"");
+			}
+		});
+	}
+	
+	private boolean IsInWhiteList(String target)
+	{
+		for(String s : whiteList)
+		{
+			if(s.equals(target))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
